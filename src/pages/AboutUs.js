7771c3b9f29cca6d9fe65c98632bd1342c3a4960
james reantillo <james.reantillo@gmail.com>
2020-11-23 00:34:12 -0800

    add about section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//Page Component
+import AboutSection from '../components/AboutSection';
+
+const AboutUs = () => {
+  return <AboutSection />;
+};
+
+export default AboutUs;
