174f6964e95c965ba956303e3049971d3813c29c
james reantillo <james.reantillo@gmail.com>
2020-11-23 21:40:44 -0800

    style about section update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import AboutSection from './components/AboutSection';
+//Global Style
+import GlobalStyle from './components/GlobalStyle';
 //Import Pages
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className='App'>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
