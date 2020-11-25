56d41f57d48c99e4063d0b6f3e047779a7b195c2
james reantillo <james.reantillo@gmail.com>
2020-11-25 00:33:54 -0800

    faq section style update

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 // Styles
-import styled from 'styled-components'
-import {About} from '../styles'
+import styled from 'styled-components';
+import { About } from '../styles';
 
 const FaqSection = () => {
   return (
@@ -9,51 +9,79 @@ const FaqSection = () => {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
+      <div className='question'>
         <h4>How Do I Start?</h4>
-        <div className="answer">
+        <div className='answer'>
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             Necessitatibus, neque.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-      <div className="question">
+      <div className='question'>
         <h4>Daily Schedule</h4>
-        <div className="answer">
+        <div className='answer'>
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             Necessitatibus, neque.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-      <div className="question">
+      <div className='question'>
         <h4>Diferrent Payment Methods</h4>
-        <div className="answer">
+        <div className='answer'>
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             Necessitatibus, neque.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-      <div className="question">
+      <div className='question'>
         <h4>What Products do you offer.</h4>
-        <div className="answer">
+        <div className='answer'>
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             Necessitatibus, neque.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
     </Faq>
   );
 };
 
 const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
 `;
 
 export default FaqSection;
