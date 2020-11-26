3786b7116b8204d0398a2c1ebedc529ab449a58e
james reantillo <james.reantillo@gmail.com>
2020-11-25 23:45:47 -0800

    partial styling of nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,9 @@ const Nav = () => {
   return (
     <StyledNav>
       <h1>
-        <a href='#'>Capture</a>
+        <a id='logo' href='#'>
+          Capture
+        </a>
       </h1>
       <ul>
         <li>
@@ -30,6 +32,24 @@ const StyledNav = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 1rem 10rem;
+  background-color: #282828;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  #logo {
+    font-size: 1.5rem;
+    font-family: 'Lobster', cursive;
+    font-weight: lighter;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
 `;
 
 export default Nav;
