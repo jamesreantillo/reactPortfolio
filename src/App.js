7af95d1a78fac1f52a91a73cb59c3bcc42486f21
james reantillo <james.reantillo@gmail.com>
2020-12-04 01:26:47 -0800

    update movie detail

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
 import ContactUs from './pages/ContactUs';
 import OurWork from './pages/OurWork';
-//Import Nav
 import Nav from './components/Nav';
+import MovieDetail from './pages/MovieDetail';
 //Router
 import { Switch, Route } from 'react-router-dom';
 
@@ -19,8 +19,11 @@ function App() {
         <Route path='/' exact>
           <AboutUs />
         </Route>
-        <Route path='/work'>
+        <Route path='/work' exact>
           <OurWork />
+        </Route>
+        <Route path='/work/:id'>
+          <MovieDetail />
         </Route>
         <Route path='/contact'>
           <ContactUs />
