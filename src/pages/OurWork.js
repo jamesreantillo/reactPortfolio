a82c92616608bf7c514bd95ff8f1fccd631f9b14
james reantillo <james.reantillo@gmail.com>
2020-12-05 02:36:03 -0800

    add transition to all pages

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -5,10 +5,13 @@ import { Link } from 'react-router-dom';
 import athlete from '../img/athlete-small.png';
 import theRacer from '../img/theracer-small.png';
 import goodTimes from '../img/goodtimes-small.png';
+//Animation
+import { motion } from 'framer-motion';
+import { pageAnimation } from '../animation';
 
 const OurWork = () => {
   return (
-    <Work>
+    <Work variants={pageAnimation} initial='hidden' animate='show' exit='exit'>
       <Movie>
         <h2>The Athlete</h2>
         <div className='line'></div>
@@ -33,7 +36,7 @@ const OurWork = () => {
     </Work>
   );
 };
-const Work = styled.div`
+const Work = styled(motion.div)`
   min-height: 100vh;
   overflow: hidden;
   padding: 5rem 10rem;
