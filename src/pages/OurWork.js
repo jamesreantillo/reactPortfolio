060dc817833f5b07d291733a9ceb0695dadd08cf
james reantillo <james.reantillo@gmail.com>
2020-12-06 15:29:40 -0800

    about animation

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -11,7 +11,13 @@ import { pageAnimation } from '../animation';
 
 const OurWork = () => {
   return (
-    <Work variants={pageAnimation} initial='hidden' animate='show' exit='exit'>
+    <Work
+      variants={pageAnimation}
+      initial='hidden'
+      animate='show'
+      exit='exit'
+      style={{ background: '#fff' }}
+    >
       <Movie>
         <h2>The Athlete</h2>
         <div className='line'></div>
